d8e7f06e961fe30861c1ad7259ea87ef70254c51
Ralf Becher <ralf.becher@web.de>
2016-04-13 13:20:33 +0200

    dirty workaround for mashups
    issue with require.js and CORS on loading text files

--- src/properties.js
+++ src/properties.js
@@ -1,149 +1,139 @@
-define( [
-    'text!./config/layouts.json'
-], function ( layouts ) {
-    'use strict';
+define(["jquery"], function ($) {
+    "use strict";
 
-    // ****************************************************************************************
-    // KPI data
-    // ****************************************************************************************
+    var layouts = {
+        "default": {
+            "name": "Default",
+            "tileBackgroundColor": "#F8F8F8",
+            "titleColor": "#333333",
+            "comparisonPositiveColor": "#006600",
+            "comparisonNegativeColor": "#CC0000",
+            "comparisonNeutralColor": "#333333",
+            "comparisonPositiveIcon": "&#9650;",
+            "comparisonNegativeIcon": "&#9660;",
+            "comparisonNeutralIcon": "&#9654;"
+        },
+        "green": {
+            "name": "Green",
+            "tileBackgroundColor": "#91C526",
+            "titleColor": "#fff",
+            "comparisonPositiveColor": "#006600",
+            "comparisonNegativeColor": "#CC0000",
+            "comparisonNeutralColor": "#333",
+            "comparisonPositiveIcon": "&#9650;",
+            "comparisonNegativeIcon": "&#9660;",
+            "comparisonNeutralIcon": "&#9654;"
+        },
+        "orange": {
+            "name": "Orange",
+            "tileBackgroundColor": "#FFB304",
+            "titleColor": "#fff",
+            "comparisonPositiveColor": "#006600",
+            "comparisonNegativeColor": "#CC0000",
+            "comparisonNeutralColor": "#333",
+            "comparisonPositiveIcon": "&#9650;",
+            "comparisonNegativeIcon": "&#9660;",
+            "comparisonNeutralIcon": "&#9654;"
+        }
+    };
+
 
     var kpiTitle = {
-        ref: "props.kpiTitle",
-        label: "KPI title",
-        type: "string",
-        expression: "optional"
-    };
-
-    var kpi = {
-        ref: "props.kpi",
-        label: "KPI",
-        type: "string",
-        expression: "optional"
-    };
-
-    var kpiComparisonLabel = {
-        ref: "props.kpiComparisonLabel",
-        label: "KPI comparison (displayed value)",
-        type: "string",
-        expression: "optional"
-    };
-
-    var kpiComparison = {
-        ref: "props.kpiComparison",
-        label: "KPI comparison (numeric value)",
-        type: "number",
-        expression: "optional"
-    };
-
-    // ****************************************************************************************
-    // General Layout Settings
-    // ****************************************************************************************
-
-    var layoutMode = {
-        ref: "props.layoutMode",
-        label: "Layout",
-        type: "string",
-        component: "radiobuttons",
-        options: [
-            {
+            ref: "props.kpiTitle",
+            label: "KPI title",
+            type: "string",
+            expression: "optional"
+        },
+        kpi = {
+            ref: "props.kpi",
+            label: "KPI",
+            type: "string",
+            expression: "optional"
+        },
+        kpiComparisonLabel = {
+            ref: "props.kpiComparisonLabel",
+            label: "KPI comparison (displayed value)",
+            type: "string",
+            expression: "optional"
+        },
+        kpiComparison = {
+            ref: "props.kpiComparison",
+            label: "KPI comparison (numeric value)",
+            type: "number",
+            expression: "optional"
+        },
+        layoutMode = {
+            ref: "props.layoutMode",
+            label: "Layout",
+            type: "string",
+            component: "radiobuttons",
+            options: [{
                 value: "default",
                 label: "Default Layout"
-            },
-            {
+            }, {
                 value: "custom",
                 label: "Custom Layout"
-            },
-            {
+            }, {
                 value: "template",
                 label: "Template"
-            }
-        ],
-        defaultValue: "default"
-    };
-
-    var options = [];
-    _.map( JSON.parse( layouts ), function ( item, key ) {
+            }],
+            defaultValue: "default"
+        },
+        options = [];
+    _.map(layouts, function (item, key) {
         var o = {
             value: key,
             label: item.name
         };
-        options.push( o );
-    } );
-
+        options.push(o)
+    });
     var layoutTemplate = {
-        ref: "props.layoutTemplate",
-        label: "Template",
-        type: "string",
-        component: "dropdown",
-        options: options,
-        defaultValue: "default",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode && data.props.layoutMode === 'template';
-        }
-
-    };
-
-    // ****************************************************************************************
-    // Custom Layout
-    // ****************************************************************************************
-
-    var tileBackgroundColor = {
-        ref: "props.tileBackgroundColor",
-        label: "Tile background color",
-        type: "string",
-        expression: "optional",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode === 'custom';
-        }
-    };
-
-    var titleColor = {
-        ref: "props.titleColor",
-        label: "Title color",
-        type: "string",
-        expression: "optional",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode === 'custom';
-        }
-    };
-
-    var kpiColor = {
-        ref: "props.kpiColor",
-        label: "KPI color",
-        type: "string",
-        expression: "optional",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode === 'custom';
-        }
-
-    };
-
-    var comparisonColor = {
-        ref: "props.comparisonColor",
-        label: "Comparison color",
-        type: "string",
-        expression: "optional",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode === 'custom';
-        }
-
-    };
-
-    var comparisonSymbol = {
-        ref: "props.comparisonSymbol",
-        label: "Comparison symbol",
-        type: "string",
-        expression: "optional",
-        show: function ( data ) {
-            return data.props && data.props.layoutMode === 'custom';
-        }
-    };
-
-    // ****************************************************************************************
-    // Property Panel Definition
-    // ****************************************************************************************
-
-    // Return values
+            ref: "props.layoutTemplate",
+            label: "Template",
+            type: "string",
+            component: "dropdown",
+            options: options,
+            defaultValue: "default",
+            show: function (data) {
+                return data.props && data.props.layoutMode && "template" === data.props.layoutMode
+            }
+        },
+        tileBackgroundColor = {
+            ref: "props.tileBackgroundColor",
+            label: "Tile background color",
+            type: "string",
+            expression: "optional",
+            show: function (data) {
+                return data.props && "custom" === data.props.layoutMode
+            }
+        },
+        titleColor = {
+            ref: "props.titleColor",
+            label: "Title color",
+            type: "string",
+            expression: "optional",
+            show: function (data) {
+                return data.props && "custom" === data.props.layoutMode
+            }
+        },
+        kpiColor = {
+            ref: "props.kpiColor",
+            label: "KPI color",
+            type: "string",
+            expression: "optional",
+            show: function (data) {
+                return data.props && "custom" === data.props.layoutMode
+            }
+        },
+        comparisonColor = {
+            ref: "props.comparisonColor",
+            label: "Comparison color",
+            type: "string",
+            expression: "optional",
+            show: function (data) {
+                return data.props && "custom" === data.props.layoutMode
+            }
+        };
     return {
         type: "items",
         component: "accordion",
@@ -169,6 +159,5 @@ define( [
                 }
             }
         }
-    };
-
-} );
+    }
+});
